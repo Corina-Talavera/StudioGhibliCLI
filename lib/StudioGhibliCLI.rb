8001c7a688c4b0aa8fcf18ec6be50350a843b9db
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-16 13:58:23 -0500

    Update StudioGhibliCLI.rb

--- lib/StudioGhibliCLI.rb
+++ lib/StudioGhibliCLI.rb
@@ -1,5 +1,7 @@
 require "StudioGhibliCLI/version"
-
+require "./lib/StudioGhibliCLI/cli"
+require "./lib/StudioGhibliCLI/film"
+#require the dependancies
 module StudioGhibliCLI
   class Error < StandardError; end
   # Your code goes here...
